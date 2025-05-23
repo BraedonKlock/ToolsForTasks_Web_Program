6b023d224851c82127a4b6d5b359bc5927a12fb1
Braedon Klock <kloc0004@algonquinlive.com>
2025-05-23 07:31:02 -0400

    V1.2 - I got some of the backend logic done for add and load job. Only need to add a couple features

--- JavaScript/addJob.js
+++ JavaScript/addJob.js
@@ -1,10 +1,46 @@
-document.addEventListener('DOMContentLoaded', domReady);
 
-function domReady() {
-    const mainMenuBtn = document.getElementById('mainMenuBtn');
-    mainMenuBtn.addEventListener('click', goToMainMenu);
+function addJob() {
+    const name = document.getElementById("jobName").value.trim();
+    const notes = document.getElementById("jobNotes").value.trim();
+
+    const job = {
+        name: name,
+        notes: notes
+    }
+    
+    const jobList = JSON.parse(localStorage.getItem("jobList")) || [];
+    jobList.push(job);
+    localStorage.setItem("jobList", JSON.stringify(jobList));
+
+    document.getElementById("jobName").value = "";
+    document.getElementById("jobNotes").value = "";
+
+    alert("Job added!");
 }
 
-function goToMainMenu() {
-    window.location.href = '../index.html'
+function loadJobs() {
+    const jobList = JSON.parse(localStorage.getItem("jobList")) || [];
+    const jobDisplay = document.getElementById("jobDisplay");
+    jobDisplay.innerHTML = "";
+    
+    for (let i = 0; i < jobList.length; i++) {
+        const job = jobList[i];
+
+        const div = document.createElement("div");
+        div.setAttribute("class", "job");
+
+        const name = document.createElement("p");
+        name.setAttribute("class", "JobInfo");
+        name.textContent = job.name;
+
+        const notes = document.createElement("p");
+        notes.setAttribute("class", "jobInfo");
+        notes.textContent = job.notes;
+
+        div.appendChild(name);
+        div.appendChild(notes);
+
+        jobDisplay.appendChild(div);
+
+    }
 }
